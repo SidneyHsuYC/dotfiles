885c7a915479556cbafdf16d40fe05a3865f2312
sidneyhsu <ych005@eng.ucsd.edu>
2020-05-30 17:21:38 -0700

    Add exports

--- create_links.sh
+++ create_links.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .bash_prompt .bash_profile .aliases .vimrc .vim"    # list of files/folders to symlink in homedir
+files=".bashrc .bash_prompt .bash_profile .aliases .exports .vimrc .vim"    # list of files/folders to symlink in homedir
 
 ##########
 
